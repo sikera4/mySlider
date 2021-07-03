67e9245f398f7146e0cc474f41e3409577da1e88
Andrew <sikera0812@mail.ru>
2021-07-03 18:11:29 +0300

    - make slider work with mousedown on the base - add min, max and default value - create a tooltip for current value

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,61 @@
 import './scss/index.scss';
 
-let thumb = $('.thumb');
+let $thumb: JQuery = $('.thumb');
+let $slider: JQuery = $('.slider');
+let $min: JQuery = $('.min');
+let $max: JQuery = $('.max');
+let $tooltip: JQuery = $('.tooltip');
+let min: number = 20;
+let max: number = 100;
+let dflt: number = 20;
+let sliderWidth: number = $slider[0].getBoundingClientRect().right - $slider[0].getBoundingClientRect().left;
 
-thumb.on('mousedown', thumbMouseDownHandler)
+$thumb.css('left', `${sliderWidth/(max-min)*(dflt - min)}px`)
+$min.html(`${min}`);
+$max.html(`${max}`);
+$tooltip.html(`${dflt}`);
 
-function thumbMouseDownHandler(event: JQuery.Event) {
-  const slider = $('#slider');
-  thumb.css({
-    'position': 'absolute',
-    'z-index': '1000',
-  });
-  moveAt(event.pageX);
-  console.log('lol');
-  slider.on('mousemove', sliderMouseMoveHandler);
-  thumb.on('mouseup', thumbMouseUpHandler);
+$thumb.on('mousedown', MouseDownHandler)
+$slider.on('mousedown', MouseDownHandler)
+
+function MouseDownHandler(event: JQuery.TriggeredEvent) {
+  
+  const $document = $(document);
+  event.preventDefault();
+  if ($(event.target).attr('class') === 'slider') {
+    let newCoor = event.clientX - $slider[0].getBoundingClientRect().left - $thumb.outerWidth()/2;
+    $thumb.css('left', `${event.clientX - $slider[0].getBoundingClientRect().left - $thumb.outerWidth()/2}px`);
+    $tooltip.html(`${newCoor/sliderWidth*(max - min) + min}`);
+  }
+  
+  let shiftX = event.clientX - $thumb[0].getBoundingClientRect().left;
+  $document.on('mousemove', mouseMoveHandler);
+  $document.on('mouseup', mouseUpHandler);
+
+  function mouseMoveHandler(event: JQuery.Event) {
+    let newCoor = event.clientX - shiftX - $slider[0].getBoundingClientRect().left;
+
+    if (newCoor < 0) {
+      newCoor = 0;
+    }
+
+    let rightSide = $slider.outerWidth();
+
+    if (newCoor > rightSide) {
+      newCoor = rightSide;
+    }
+    
+    $thumb.css('left', `${newCoor}px`);
+    $tooltip.html(`${(newCoor/sliderWidth*(max - min) + min).toFixed(2)}`);
+  }
+
+  function mouseUpHandler(event: JQuery.Event) {
+    $document.off();
+  }
+  
 }
 
-function moveAt(pageX: number) {
-  thumb.css('left', `${pageX - thumb.outerWidth()/2}px`)
-}
 
-function sliderMouseMoveHandler(event: JQuery.Event) {
-  moveAt(event.pageX);
-}
 
-function thumbMouseUpHandler(event: JQuery.Event) {
-  $('#slider').off();
-  thumb.off('mouseup');
-}
+
+
