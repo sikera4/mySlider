c391388f3147b3bacaabc572dd8bb7a134500f80
Andrew <sikera0812@mail.ru>
2021-07-05 20:35:34 +0300

    - add step functionality - add progress bar

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import './scss/index.scss';
 
-let $thumb: JQuery = $('.thumb');
-let $slider: JQuery = $('.slider');
-let $min: JQuery = $('.min');
-let $max: JQuery = $('.max');
-let $tooltip: JQuery = $('.tooltip');
+const $thumb: JQuery = $('.thumb');
+const $slider: JQuery = $('.slider');
+const $min: JQuery = $('.min');
+const $max: JQuery = $('.max');
+const $tooltip: JQuery = $('.tooltip');
+const $progressBar: JQuery = $('.progress-bar');
+
 let min: number = 20;
 let max: number = 100;
-let dflt: number = 20;
+let dflt: number = 50;
+let step: number = 13;
 let sliderWidth: number = $slider[0].getBoundingClientRect().right - $slider[0].getBoundingClientRect().left;
 
 $thumb.css('left', `${sliderWidth/(max-min)*(dflt - min)}px`)
 $min.html(`${min}`);
 $max.html(`${max}`);
 $tooltip.html(`${dflt}`);
-
+$progressBar.css('width', `${sliderWidth/(max-min)*(dflt - min)}`)
 $thumb.on('mousedown', MouseDownHandler)
 $slider.on('mousedown', MouseDownHandler)
 
@@ -22,15 +25,40 @@ function MouseDownHandler(event: JQuery.TriggeredEvent) {
   
   const $document = $(document);
   event.preventDefault();
-  if ($(event.target).attr('class') === 'slider') {
-    let newCoor = event.clientX - $slider[0].getBoundingClientRect().left - $thumb.outerWidth()/2;
-    $thumb.css('left', `${event.clientX - $slider[0].getBoundingClientRect().left - $thumb.outerWidth()/2}px`);
-    $tooltip.html(`${newCoor/sliderWidth*(max - min) + min}`);
+  if ($(event.target).attr('class') === 'slider' || $(event.target).attr('class') === 'progress-bar') {
+    let newCoor = event.clientX - $slider[0].getBoundingClientRect().left;
+    let value = newCoor/sliderWidth * (max - min) + min;
+    let possibleValue = closestPossibleValueFinder(value);
+    let coor = (possibleValue - min)/(max - min) * sliderWidth;
+
+    $thumb.css('left', `${coor}px`);
+    $progressBar.css('width', `${coor}`);
+    $tooltip.html(`${possibleValue}`);
   }
   
   let shiftX = event.clientX - $thumb[0].getBoundingClientRect().left;
   $document.on('mousemove', mouseMoveHandler);
   $document.on('mouseup', mouseUpHandler);
+  
+  function possibleValuesCreator() {
+    let possibleValues: number[] = [];
+    for (let value = dflt; value >= min; value -= step) {
+      possibleValues.push(value);
+    }
+    for (let value = dflt + step; value <= max; value += step) {
+      possibleValues.push(value);
+    }
+    
+    return possibleValues;
+  }
+
+  function closestPossibleValueFinder(value: number) {
+    const possibleValues = possibleValuesCreator();
+    let closestValue = possibleValues.reduce((prev: number, curr: number):number => {
+      return (Math.abs(curr - value) < Math.abs(prev - value) ? curr : prev);
+    });
+    return closestValue;
+  }
 
   function mouseMoveHandler(event: JQuery.Event) {
     let newCoor = event.clientX - shiftX - $slider[0].getBoundingClientRect().left;
@@ -44,15 +72,17 @@ function MouseDownHandler(event: JQuery.TriggeredEvent) {
     if (newCoor > rightSide) {
       newCoor = rightSide;
     }
-    
-    $thumb.css('left', `${newCoor}px`);
-    $tooltip.html(`${(newCoor/sliderWidth*(max - min) + min).toFixed(2)}`);
+    let value = newCoor/sliderWidth * (max - min) + min;
+    let possibleValue = closestPossibleValueFinder(value);
+    let coor = (possibleValue - min)/(max - min) * sliderWidth;
+    $thumb.css('left', `${coor}px`);
+    $progressBar.css('width', `${coor}`);
+    $tooltip.html(`${possibleValue}`);
   }
 
   function mouseUpHandler(event: JQuery.Event) {
     $document.off();
   }
-  
 }
 
 
